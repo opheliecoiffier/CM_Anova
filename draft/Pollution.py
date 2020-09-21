22e3dd905aa591558a5971e09aab981f75d46fe9
opheliecoiffier <60701868+opheliecoiffier@users.noreply.github.com>
2020-09-21 15:24:05 +0200

    pollution 's graph

--- draft/Pollution.py
+++ draft/Pollution.py
@@ -6,27 +6,29 @@ from download import download
 import statsmodels.api as sm
 import scipy as sp
 from statsmodels.formula.api import ols
+sns.set_palette("colorblind")
+sns.set()
 
 url = "http://josephsalmon.eu/enseignement/datasets/Mesure_journaliere_Region_Occitanie_Polluants_Principaux.csv"
 path_target = "./Mesure_journaliere_Region_Occitanie_Polluants_Principaux.csv"
 download(url, path_target, replace=False)
 
 #%%
+#new data
 df = pd.read_csv('Mesure_journaliere_Region_Occitanie_Polluants_Principaux.csv')
-df= df.loc[:,["polluant", 'nom_com', "valeur_originale"]]
+df= df.loc[:, ["polluant", 'nom_com', "valeur_originale"]]
 #creation du tableau ne contenant que ces villes et O3 comme polluant
 df = df.loc[df["nom_com"].isin(["MONTPELLIER","NIMES", "TARBES", "CASTRES"])]
 df = df.loc[df["polluant"].isin(["O3"])]
-print(df["polluant"])
 
 #violins : descriptive analysis
 sns.catplot(x=df.columns[1], y="valeur_originale",
          data=df, kind="violin", legend=False)
-plt.title("O3 par ville")
+plt.title("O3 by city")
+plt.xlabel("cities")
+plt.ylabel("Concentration of O3")
 plt.legend(loc=1)
 plt.tight_layout()
-
-print(df.head())
 
 #%%
 #descriptive analysis
